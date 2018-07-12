2cc986b7cd3e77c5093045e57e154235fc8662ae
Zul Hilmi <netspytux@gmail.com>
2018-07-12 07:45:42 +0700

    Change Database Engine

--- prakerin/settings.py
+++ prakerin/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'prakerin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
