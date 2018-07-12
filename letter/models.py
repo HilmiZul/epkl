9be6e7ec5a7fad1ad5b9a6f4950869366bacba5b
Zul Hilmi <netspytux@gmail.com>
2018-07-12 07:33:30 +0700

    menambah model untuk surat permohonan TKJ

--- letter/models.py
+++ letter/models.py
@@ -1,13 +1,21 @@
 from __future__ import unicode_literals
 from django.db import models
 
-from master.models import Siswa, Instansi
+from master.models import Siswa, Instansi, InstansiTKJ
 
 # Create your models here.
 class Permohonan(models.Model):
     perihal = models.CharField(max_length=200, default=None)
     nama_siswa = models.ForeignKey(Siswa)
     nama_instansi = models.ForeignKey(Instansi)
+
+    def __unicode__(self):
+        return self.perihal
+
+class PermohonanTKJ(models.Model):
+    perihal = models.CharField(max_length=200, default=None)
+    nama_siswa = models.ForeignKey(Siswa)
+    nama_instansi = models.ForeignKey(InstansiTKJ)
 
     def __unicode__(self):
         return self.perihal
