a3b5c10e734a986c6a01fbed55dfaa160bb24a3e
Zul Hilmi <netspytux@gmail.com>
2018-07-12 07:33:51 +0700

    menambah admin untuk surat permohonan TKJ

--- letter/admin.py
+++ letter/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from letter.models import Permohonan
+from letter.models import Permohonan, PermohonanTKJ
 
 # Register your models here.
 class PermohonanAdmin(admin.ModelAdmin):
@@ -9,4 +9,11 @@ class PermohonanAdmin(admin.ModelAdmin):
     list_filter = ('nama_instansi',)
     list_per_page = 20
 
+class PermohonanTKJAdmin(admin.ModelAdmin):
+    list_display = ['nama_siswa', 'nama_instansi']
+    search_fields = ['nama_siswa']
+    list_filter = ('nama_instansi',)
+    list_per_page = 20
+
 admin.site.register(Permohonan, PermohonanAdmin)
+admin.site.register(PermohonanTKJ, PermohonanTKJAdmin)
