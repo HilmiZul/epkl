06c412f1f646d681181cdf716ae82455f55f4030
Zul Hilmi <netspytux@gmail.com>
2018-08-17 20:09:07 +0700

    first commit

---
+++ master/form_tkj.py
@@ -0,0 +1,36 @@
+from django.forms import ModelForm
+from django import forms
+from master.models import Siswa
+
+class FormTKJ(ModelForm):
+    class Meta:
+        model = Siswa
+        fields = ['NIS', 'nama', 'kelas']
+
+        labels = {
+          'NIS':"NIS",
+          'nama':'NAMA',
+          'kelas':'KELAS',
+        }
+
+        widgets = {
+            'NIS': forms.TextInput(attrs={
+                'type': 'text',
+                'class': 'form-control',
+                'placeholder': 'Masukkan NIS',
+                'required': 'required',
+                'autofocus': 'autofocus',
+                }),
+            'nama': forms.TextInput(attrs={
+                'type': 'text',
+                'class': 'form-control',
+                'placeholder': 'Masukkan Nama',
+                'required': 'required',
+                }),
+            'kelas': forms.Select(attrs={
+                'class': 'form-control selectpicker',
+                'data-live-search': 'true',
+                'required': 'required',
+                }),
+
+        }
